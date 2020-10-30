99fe00294ff15ba30e6a61d53e54883ea472c59e
Jayakrishnan GK <jayakrishnan.gk@elucidata.io>
2020-10-31 01:35:56 +0530

    Null safe date reformat function

--- utils.py
+++ utils.py
@@ -3,9 +3,14 @@ import datetime
 
 def reformat_date_str(date_str, date_format="%Y-%m-%d",
                       target_date_format="%b %d, %Y"):
-    return datetime.datetime.strptime(
-        date_str, date_format).strftime(target_date_format)
+    formatted_date_str = "-"
+    try:
+        formatted_date_str = datetime.datetime.strptime(
+            date_str, date_format).strftime(target_date_format)
+    except Exception as e:
+        print(f"Error during date conversion: {e})")
+    return formatted_date_str
 
 
 if __name__ == "__main__":
-    print(reformat_date_str("2020-11-02"))
+    print(reformat_date_str("2020-06-05"))
